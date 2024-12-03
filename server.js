7d57ad0ddb467047f887da74b14b23f124077c1f
manoxx <mbpi.obpl@gmail.com>
2024-12-03 20:06:16 +0530

    updated

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
-app.use(cors({origin:'https://mern-assi-frontend.vercel.app/'}));
+app.use(cors({origin:'*'}));
+// app.use(cors({origin:'https://mern-assi-frontend.vercel.app/'}));
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));//for serve uploaded images
 app.use('/api/admin', adminRoutes);
